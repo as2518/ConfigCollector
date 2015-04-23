03283a5e31be8269c07337eda530e4c9f9c76540
t-tsuchiya <t-tsuchiya@biglobe.co.jp>
2015-04-23 13:39:30 +0900

    retake commit push

---
+++ example.py
@@ -0,0 +1,18 @@
+from git_router import Router
+
+ipv4 = '192.168.10.1'
+username = 'test'
+password = 'test'
+os = 'JUNOS'
+
+
+router = Router(ipv4, username, password, os)
+
+router.login()
+
+router_config = router.get_config()
+hostname = router.get_hostname()
+
+router.upload_config( router_config )
+router.logout()
+
--- git_router.py
+++ git_router.py
@@ -1,56 +1,62 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import git
+from git import Repo
 from Exscript.protocols import SSH2
 from Exscript.Account import Account
 
+class Router:
+    def __init__(self, ipv4, username, password, os):
+        self.ipv4 = ipv4
+        self.username = username
+        self.password = password
+        self.os = os
 
-def login(self, ipv4, username, password):
-    session = SSH2()
-    session.connect(ipv4)
-    session.login( Account(username, password) )
-    return session
+    def login(self):
+        self.session = SSH2()
+        self.session.connect(self.ipv4)
+        self.session.login( Account(self.username, self.password) )
+        return self.session
 
-def logout(self, session):
-    session.send('exit\r')
-    session.close()
+    def logout(self):
+        self.session.send('exit\r')
+        self.session.close()
 
-def get_config(self, session, os):
-    result = 'N/A'
+    def get_config(self):
+        result = 'N/A'
 
-    if( os == 'IOS-XR' ):
-        session.execute('terminal length 0')
-        session.excute('show running-config')
-        result = session.response.splitlines()
-    elif( os == 'JUNOS'):
-        session.excute('show configuration | no-more')
-        result = session.response.splitlines()
-    else:
+        if( self.os == 'IOS-XR' ):
+            self.session.execute('terminal length 0')
+            self.session.excute('show running-config')
+            result = self.session.response.splitlines()
+        elif( self.os == 'JUNOS'):
+            self.session.excute('show configuration | no-more')
+            result = self.session.response.splitlines()
+        else:
+            pass
+
+        return result
+
+    def get_hostname(self):
+        hostname = 'N/A'
+        result = 'N/A'
+        if( self.os == 'IOS-XR' ):
+            self.session.excute('show running-config | incude hostname')
+            result = self.session.response.splitlines()
+
+            #implement later
+            hostname = result
+        elif( self.os == 'JUNOS'):
+            self.session.excute('show configuration | match host-name')
+            result = self.session.response.splitlines()
+
+            #implement later
+            hostname = result
+        else:
+            pass
+
+        return hostname
+
+
+    def upload_config(self, config_file):
         pass
-
-    return result
-
-def get_hostname(self, session, os):
-    hostname = 'N/A'
-    result = 'N/A'
-    if( os == 'IOS-XR' ):
-        session.excute('show running-config | incude hostname')
-        result = session.response.splitlines()
-
-        #implement later
-        hostname = result
-    elif( os == 'JUNOS'):
-        session.excute('show configuration | match host-name')
-        result = session.response.splitlines()
-
-        #implement later
-        hostname = result
-    else:
-        pass
-
-    return hostname
-
-
-def upload_config(self, git_repository):
-    pass
